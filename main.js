c9e57cfe29236d87de81bcba254bfa431b5d4390
Christian Droulers <droulers.christian@gmail.com>
2021-02-14 22:05:17 -0500

    Add some VSCode stuff, format files.

--- main.js
+++ main.js
@@ -1,58 +1,66 @@
 // Amelie Official Title: Reverse Engineer
-const startingWidth = innerWidth/1||document.documentElement.clientWidth/1||document.body.clientWidth/1;
-const startingHeight = innerHeight/1||document.documentElement.clientHeight/1||document.body.clientHeight/1;
-
+const startingWidth =
+  innerWidth / 1 || document.documentElement.clientWidth / 1 || document.body.clientWidth / 1;
+const startingHeight =
+  innerHeight / 1 || document.documentElement.clientHeight / 1 || document.body.clientHeight / 1;
 
 d3.select(window).on("resize", main);
 
 // no shringaling
 function responsiveReposition() {
+  let currentWidth =
+    innerWidth / 1 || document.documentElement.clientWidth / 1 || document.body.clientWidth / 1;
 
-  let currentWidth = innerWidth/1||document.documentElement.clientWidth/1||document.body.clientWidth/1;
+  let currentHeight =
+    innerHeight / 1 || document.documentElement.clientHeight / 1 || document.body.clientHeight / 1;
 
-  let currentHeight = innerHeight/1||document.documentElement.clientHeight/1||document.body.clientHeight/1;
+  let xOffset = -(startingWidth - currentWidth) / 2;
+  let yOffset = -(startingHeight - currentHeight) / 2;
 
-  let xOffset = -(startingWidth-currentWidth)/2;
-  let yOffset = -(startingHeight-currentHeight)/2;
-  
-  d3.select("body").select("g")
-      .style("transform", `translate(${xOffset}px, ${yOffset}px) scale(${1})`); // this uses a "template" string
-  d3.select("svg")
-    .attr("width", currentWidth)
-    .attr("height", currentHeight)
+  d3.select("body")
+    .select("g")
+    .style("transform", `translate(${xOffset}px, ${yOffset}px) scale(${1})`); // this uses a "template" string
+  d3.select("svg").attr("width", currentWidth).attr("height", currentHeight);
 }
 
 main();
 
 function main() {
-
   let svgtest = d3.select("body").select("svg");
-  if (!svgtest.empty()){
+  if (!svgtest.empty()) {
     svgtest.remove();
   }
-  let width = innerWidth/1||document.documentElement.clientWidth/1||document.body.clientWidth/1;
-  
-  let height = innerHeight/1||document.documentElement.clientHeight/1||document.body.clientHeight/1;
+  let width =
+    innerWidth / 1 || document.documentElement.clientWidth / 1 || document.body.clientWidth / 1;
+
+  let height =
+    innerHeight / 1 || document.documentElement.clientHeight / 1 || document.body.clientHeight / 1;
 
   let root;
 
-  let canvas = d3.select("body")
-              .append("svg")
-              .attr("width", width)
-              .attr("height", height)
-              .append("g")
-              .style("transform", "translate(0, 0)");
+  let canvas = d3
+    .select("body")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g")
+    .style("transform", "translate(0, 0)");
 
-  let force = d3.layout.force()
+  let force = d3.layout
+    .force()
     .size([width, height])
-    .charge(function(d) { return d._children ? -d.size / 100 : -innerWidth/5; })
-    .linkDistance(function(d) { return d.target._children ? 80 : 25 || innerHeight; })
+    .charge(function (d) {
+      return d._children ? -d.size / 100 : -innerWidth / 5;
+    })
+    .linkDistance(function (d) {
+      return d.target._children ? 80 : 25 || innerHeight;
+    })
     .on("tick", tick);
 
   let link = canvas.selectAll(".link");
   let node = canvas.selectAll(".node");
 
-  d3.json("oeuvres.json", function(error, json) {
+  d3.json("oeuvres.json", function (error, json) {
     if (error) throw error;
     root = json;
     update();
@@ -66,115 +74,112 @@ function main() {
     let links = d3.layout.tree().links(nodes);
 
     // Restart the force layout.
-    force
-      .nodes(nodes)
-      .links(links)
-      .start();
+    force.nodes(nodes).links(links).start();
 
     // Update the links
-    link = link.data(links, d => d.target.id);
+    link = link.data(links, (d) => d.target.id);
 
     // Exit any old links.
     link.exit().remove();
 
     // Enter any new links.
-    link.enter()
-          .insert("line", ".node")
-          .attr("class", "link")
-          .attr("x1", d => d.source.x)
-          .attr("y1", d => d.source.y)
-          .attr("x2", d => d.target.x)
-          .attr("y2", d => d.target.y);
+    link
+      .enter()
+      .insert("line", ".node")
+      .attr("class", "link")
+      .attr("x1", (d) => d.source.x)
+      .attr("y1", (d) => d.source.y)
+      .attr("x2", (d) => d.target.x)
+      .attr("y2", (d) => d.target.y);
 
     // Update the nodes
-    node = node.data(nodes, d => d.id).style("fill", color);
+    node = node.data(nodes, (d) => d.id).style("fill", color);
 
     // Exit any old nodes.
     node.exit().remove();
 
     // Enter any new nodes.
-    node.enter()
-          .append("circle")
-            .attr("class", "node")
-            .attr("cx", d => d.x)
-            .attr("cy", d => d.y)
-            .attr("r", d => Math.sqrt(d.size) / 3 || 10)
-            .attr("fill", color)
-            .on("click", toggleChildren)
-            .on("mouseover", showImage)
-            .on("mouseout", hideImage)
-            .call(force.drag)
+    node
+      .enter()
+      .append("circle")
+      .attr("class", "node")
+      .attr("cx", (d) => d.x)
+      .attr("cy", (d) => d.y)
+      .attr("r", (d) => Math.sqrt(d.size) / 3 || 10)
+      .attr("fill", color)
+      .on("click", toggleChildren)
+      .on("mouseover", showImage)
+      .on("mouseout", hideImage)
+      .call(force.drag);
 
     canvas.append("svg:image").attr("class", "hoverimg");
     canvas.append("svg:text").attr("class", "hovertxt");
-    
   }
 
   function showImage(d) {
-
     d3.select("text")
-        .attr('transform', `translate(${d.x}, ${(d.y - 5 - (d.children ? 3.5 : Math.sqrt(d.size) / 2))})`)
-        .text(`${d.titre}`)
-        .style('display', null);
-  //  d3.select("image")
+      .attr(
+        "transform",
+        `translate(${d.x}, ${d.y - 5 - (d.children ? 3.5 : Math.sqrt(d.size) / 2)})`
+      )
+      .text(`${d.titre}`)
+      .style("display", null);
+    //  d3.select("image")
     //    .attr('transform', 'translate(' + d.x + ',' + (d.y - 5 - (d.children ? 3.5 : Math.sqrt(d.size) / 2)) + ')')
-      //  .attr("xlink:href", d.img)
-        //.style('display', null);
-
-    
+    //  .attr("xlink:href", d.img)
+    //.style('display', null);
   }
 
   function hideImage() {
-    d3.select("text").style('display', 'none');
-  //  d3.select("image").style('display', 'none');
+    d3.select("text").style("display", "none");
+    //  d3.select("image").style('display', 'none');
   }
 
-
-
   function tick() {
-    link.attr("x1", d => d.source.x)
-        .attr("y1", d => d.source.y)
-        .attr("x2", d => d.target.x)
-        .attr("y2", d => d.target.y);
+    link
+      .attr("x1", (d) => d.source.x)
+      .attr("y1", (d) => d.source.y)
+      .attr("x2", (d) => d.target.x)
+      .attr("y2", (d) => d.target.y);
 
-    node.attr("cx", d => d.x)
-        .attr("cy", d => d.y);
+    node.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
   }
 
   // Color leaf nodes orange, and packages white or blue.
-  function color (d) {
-   if (d.year =="1999") return "#926844"
-   if (d.year == "2003") return "#6f5564"
-   if (d.year == "2004") return "#3e5e6f" 
-   if (d.year == "2005") return "#2e7572"
-   if (d.year == "2006") return "#4b624b"
-   if (d.year == "2007") return "#8a8537"
-   if (d.year == "2009") return "#5b7855"
-   if (d.year == "2012") return "#4e4656"
-   if (d.year == "2015") return "#167378"
-   if (d.year == "2016") return "#62704d"
-   if (d.year == "2017") return "#6d6942"
-   if (d.year == "2018") return "#287558"
-   if (d.year =="2019") return "#936957";
+  function color(d) {
+    if (d.year == "1999") return "#926844";
+    if (d.year == "2003") return "#6f5564";
+    if (d.year == "2004") return "#3e5e6f";
+    if (d.year == "2005") return "#2e7572";
+    if (d.year == "2006") return "#4b624b";
+    if (d.year == "2007") return "#8a8537";
+    if (d.year == "2009") return "#5b7855";
+    if (d.year == "2012") return "#4e4656";
+    if (d.year == "2015") return "#167378";
+    if (d.year == "2016") return "#62704d";
+    if (d.year == "2017") return "#6d6942";
+    if (d.year == "2018") return "#287558";
+    if (d.year == "2019") return "#936957";
   }
 
   // Toggle children on click.
   function toggleChildren(d) {
     d3.select("image")
-    .attr('transform', 'translate(' + d.x + ',' + (d.y - 5 - (d.children ? 3.5 : Math.sqrt(d.size) / 2)) + ')')
-    .attr("xlink:href", d.img)
-    .style('display', null);
-     // if (d.children) {
-      //  d._children = d.children;
-       // d.children = null;
-      //} else {
-        //d.children = d._children;
-        //d._children = null;
-      //}
-      update();
-    
+      .attr(
+        "transform",
+        "translate(" + d.x + "," + (d.y - 5 - (d.children ? 3.5 : Math.sqrt(d.size) / 2)) + ")"
+      )
+      .attr("xlink:href", d.img)
+      .style("display", null);
+    // if (d.children) {
+    //  d._children = d.children;
+    // d.children = null;
+    //} else {
+    //d.children = d._children;
+    //d._children = null;
+    //}
+    update();
   }
-
 
   // Returns a list of all nodes under the root.
   function flatten(root) {
@@ -187,8 +192,7 @@ function main() {
       nodes.push(node);
     }
 
-   recurse(root);
+    recurse(root);
     return nodes;
   }
-
 }
